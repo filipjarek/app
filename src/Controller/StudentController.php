5f503512f1eafa8a124134fc43233beee6398d0e
filipjarek <filipjarek24@gmail.com>
2023-03-02 23:43:18 +0100

    Added pagination and sorting

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use Twig\Environment;
+use App\Entity\Student;
 use App\Repository\StudentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,10 +15,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class StudentController extends AbstractController
 {
     #[Route('/students', name: 'app_student', methods: ['GET'])]
-    public function shoStudents(Environment $twig, StudentRepository $studentRepository): Response
+    public function showStudents(EntityManagerInterface $em, Environment $twig, StudentRepository $studentRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $studentRepository = $em->getRepository(Student::class);
+        $allStudentsQuery = $studentRepository->findAll();
+
+        $students = $paginator->paginate(
+            $allStudentsQuery,
+            $request->query->getInt('page', 1),
+            5
+        );
         return new Response($twig->render('student/index.html.twig', [
-            'students' => $studentRepository->findBy([], ['lastname' => 'ASC', 'firstname' => 'ASC'])
+            'students' => $studentRepository->findBy([], ['lastname' => 'ASC', 'firstname' => 'ASC']),
+            'students' => $students
         ]));
     }
 }
