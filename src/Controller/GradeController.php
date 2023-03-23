b6b38d79c8ab820963cbc339785f89ca29aa2b6b
filipjarek <filipjarek24@gmail.com>
2023-03-23 22:03:38 +0100

    Updated routes in GradeController and templates

--- src/Controller/GradeController.php
+++ src/Controller/GradeController.php
@@ -7,6 +7,7 @@ use App\Form\TaskFormType;
 use App\Repository\TaskRepository;
 use App\Repository\StudentRepository;
 use App\Repository\SubjectRepository;
+use App\Repository\ClassroomRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,14 +21,16 @@ class GradeController extends AbstractController
         protected StudentRepository $studentRepository,
         protected TaskRepository $taskRepository,
         protected SubjectRepository $subjectRepository,
+        protected ClassroomRepository $classroomRepository,
         protected EntityManagerInterface $em
     ) {
     }
 
-    #[Route('/subject/class/student/grade/delete/{id}', name: 'delete_grade', methods: ['GET', 'DELETE'])]
+    #[Route('/subject/class/{classroom_id}/student/grade/delete/{id}', name: 'delete_grade', methods: ['GET', 'DELETE'])]
     #[IsGranted('ROLE_USER')]
-    public function deleteGrade($id, EntityManagerInterface $em): Response
+    public function deleteGrade($id, $classroom_id, EntityManagerInterface $em): Response
     {
+        $classroom = $this->classroomRepository->find($classroom_id);
         $task = $this->taskRepository->find($id);
         $this->em->remove($task);
         $this->em->flush();
@@ -40,16 +43,18 @@ class GradeController extends AbstractController
         return $this->redirectToRoute('show_student', [
             'id'=>$task->getStudent()->getId(),
             'subject_id'=>$task->getSubject()->getId(),
+            'classroom_id'=>$classroom->getId()
         ]);
     }
 
-    #[Route('/subject/{subject_id}/class/student/{id}/grade/add', name: 'add_grade', methods: ['GET', 'POST'])]
+    #[Route('/subject/{subject_id}/class/{classroom_id}/student/{id}/grade/add', name: 'add_grade', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_USER')]
     #[Entity('task', expr: 'repository.find(subject_id)')]
-    public function newGrade(Request $request, EntityManagerInterface $entityManager, $id, $subject_id): Response
+    public function newGrade(Request $request, EntityManagerInterface $entityManager, $id, $subject_id, $classroom_id): Response
     {   
         $student = $this->studentRepository->find($id);
         $subject = $this->subjectRepository->find($subject_id);
+        $classroom = $this->classroomRepository->find($classroom_id);
         
         $task = new Task();
         $task->setStudent($student);
@@ -71,7 +76,8 @@ class GradeController extends AbstractController
 
             return $this->redirectToRoute('show_student', [
                 'id'=>$student->getId(),
-                'subject_id'=>$subject->getId()
+                'subject_id'=>$subject->getId(),
+                'classroom_id'=>$classroom->getId()
             ]);
         }
 
@@ -82,13 +88,14 @@ class GradeController extends AbstractController
         ]);
     }
 
-    #[Route('/subject/{subject_id}/class/student/{student_id}/grade/edit/{id}', name: 'edit_grade', methods: ['GET', 'POST'])]
+    #[Route('/subject/{subject_id}/class/{classroom_id}/student/{student_id}/grade/edit/{id}', name: 'edit_grade', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_USER')]
     #[Entity('task', expr: 'repository.find(subject_id, student_id)')]
-    public function editGrade($subject_id, $student_id, Task $task, Request $request, EntityManagerInterface $entityManager): Response
+    public function editGrade($subject_id, $student_id, $classroom_id, Task $task, Request $request, EntityManagerInterface $entityManager): Response
     {   
         $student = $this->studentRepository->find($student_id);
         $subject = $this->subjectRepository->find($subject_id);
+        $classroom = $this->classroomRepository->find($classroom_id);
 
         $form = $this->createForm(TaskFormType::class, $task);
         $form->handleRequest($request);
@@ -105,6 +112,7 @@ class GradeController extends AbstractController
             return $this->redirectToRoute('show_student', [
                 'id'=>$task->getStudent()->getId(),
                 'subject_id'=>$task->getSubject()->getId(),
+                'classroom_id'=>$classroom->getId()
             ]);
         }
 
