7d4807b66d3a736ec3a026233438b072bb4bd2bb
filipjarek <filipjarek24@gmail.com>
2023-03-27 14:35:20 +0200

    Added tests/Functional/LoginTest

---
+++ tests/Functional/LoginTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class LoginTest extends WebTestCase
+{
+    public function testLoginWithGoodCredentials(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface */
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate('app_login'));
+
+        $form = $crawler->filter('form[name=login]')->form([
+            '_username' => 'admin',
+            '_password' => 'password'
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $client->followRedirect();
+
+        $this->assertRouteSame('show_subjects');
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    public function testLoginWithWrongCredentials(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface */
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate('app_login'));
+
+        $form = $crawler->filter('form[name=login]')->form([
+            '_username' => 'adminnn',
+            '_password' => 'passwordddd'
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $client->followRedirect();
+
+        $this->assertRouteSame('app_login');
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    public function testLogout(): void
+    {
+        $client = static::createClient();
+
+        /** @var UserRepository */
+        $userRepository = $client->getContainer()->get(UserRepository::class);
+
+        /** @var UrlGeneratorInterface */
+        $urlGenerator = $client->getContainer()->get('router');
+
+        /** @var User */
+        $user = $userRepository->findOneBy([]);
+
+        $client->loginUser($user);
+
+        $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate('app_logout'));
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $client->followRedirect();
+
+        $this->assertRouteSame('app_login');
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+}
